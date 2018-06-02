dcc0e09d7088b019585eb85ea6c477aba93fcc17
User <User@DESKTOP-72T5UKA>
2018-06-02 12:49:47 +0530

    first copy

--- banking/src/banking/Banking.java
+++ banking/src/banking/Banking.java
@@ -1,87 +1,237 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *A simple Banking Application in Java which has the following features:
+*1. Add customer account (The bank supports multiple customer accounts)
+*2. Customer can deposit money into his account
+*3. Customer can withdraw money from his account
+*4. Customer can transfer money into other accounts
+*5. Calculate the total amount of money in the bank
+*6. Find the richest person in the bank.
+*/
+
+/**
+ * class Banking
+ * Makes use of ArrayList to keep track of Multiple account details
+ * Makes use of Scanner class to take the input from the users
+ 
  */
 package banking;
 
-/**
- *
- * @author User
- */
+
+import java.util.ArrayList;
 import java.util.Scanner;
-public class Banking {
-    double bal;
-    int Create(){
-        Scanner in=new Scanner(System.in);
-        System.out.println("Enter Name :" );
-        char name=in.next().charAt(0);
-        return 0;               //count                                 
-    }
-    double Deposit(){
-       Scanner in=new Scanner(System.in);
-        System.out.println("Enter Amount : " ); 
-        double damt=in.nextDouble();             //write if stmt for 500/-
-        return 0;
-    }
-    double Withdraw(){
-       Scanner in=new Scanner(System.in);
-        System.out.println("Enter withdrawl Amount : " ); 
-        double wamt=in.nextDouble();
-        
-        if((bal-wamt)<500){
-        System.out.println("Insufficient Amount" );    
-        return 0;
-        }
-        bal-=wamt;
-        System.out.println("Current Balance : " +bal );
-        return 0;
-    }
-    double Transfer(){
-        Scanner in=new Scanner(System.in);
-        System.out.println("Enter 1st acc no : " );
-        int acc1=in.nextInt();
-        System.out.println("Enter Transfer Amount : " );
-        double tamt=in.nextDouble();
-        
-        if((bal-tamt)<500){
-        System.out.println("Insufficient Amount \n Transfer fail" );    
-        return 0;
-        }
-        System.out.println("Enter 2nd acc no : " );
-        int acc2=in.nextInt();
+
+/**
+ * class Customer provides the fundamental functionalities required for each customer account in the bank
+ * 
+ */
+class Customer{
+   Scanner scan = new Scanner(System.in);
+   private int AccNo;
+   String custName,custAdd;
+   double bal;
+   
+   Customer(int AccountNo){
+      AccNo =AccountNo; 
+   }
+   
+   /**
+    * method createCustacc creates Customer Bank account
+   */
+   void createCustomerAcc() {
+        System.out.println("Your Bank Account Number is : "+ AccNo);
+        System.out.print("Enter Your Name : ");
+        custName = scan.next();
+        scan.nextLine();
+        System.out.print("Enter your Address : ");
+        custAdd = scan.nextLine();
+        bal=500;
+	System.out.println("Your Account with Account Number " +AccNo+ " is created!!");
         
     }
-    
+   
+   /**
+   *method deposite is to deposite money
+   */
+    void deposite() {
+            System.out.print("Enter the amount to be deposited : ");
+            double deposite = scan.nextDouble();
+            if(deposite > 0.0) {
+                bal +=deposite;
+                System.out.println("The balance after deposite is : "+bal);
+            }
+            else
+                System.out.println("Invalid Deposite amount!!");
+       
+    }
 
     /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        int count=0;
-        System.out.println("Welcome");
-        Scanner in=new Scanner(System.in);        
-        System.out.println("Enter your choice: " );
-        int ch=in.nextInt();
-        switch(ch){
-            case 1: Create();
-                    break;
-                    
-            case 2: Deposit();
-                    break;
-                    
-            case 3: Withdraw();
-                    break;
-                    
-            case 4: Transfer();
-                    break;
-                    
-            case 5: Total();
-                    break;
-                    
-            case 6: Rich();
-                    break;
-        }
-        }
+    *method to withdraw money
+    */
+    void withdraw() {
+        
+            System.out.print("Enter the amount to be Withdrawn : ");
+            double withdraw = scan.nextDouble();
+            
+            if((bal-withdraw)<500) {
+                System.out.println("Insufficient Bank Balance!!");
+            }
+            else{
+		bal -=withdraw;
+                System.out.println("The balance after withdrawal is : "+bal);
+		}
+        
     }
     
+}
+
+
+public class Banking {
+
+    
+   static int count=0;
+       static ArrayList<Customer> customerArray = new ArrayList<>(200);
+       static Scanner s = new Scanner(System.in);
+        
+    public static void main(String[] args) {
+        for(;;) {
+            System.out.println("1.Create Bank Account\n"
+                              +"2.Deposite\n"
+                              +"3.Withdraw\n"
+                              +"4.Transfer Money\n"
+                              +"5.Total Bank Balance\n"
+                              +"6.Know the Richest Person\n"
+                              +"7.Exit\n\n");
+            System.out.print("Enter your choice : ");
+            int choice = s.nextInt();
+            switch(choice) {
+                case 1 :   
+                            addCustomer();
+                            break;
+                case 2 : 
+                           dep();
+                           break;
+                
+                case 3 :  with();
+                           break;
+                          
+                case 4 : 
+                            transfer();
+                            break;
+                
+                case 5 : 
+                            totalBankBalance();
+                            break;
+                
+                case 6 : 
+                            richestPerson();
+                            break;
+                
+                case 7 : 
+                            System.exit(0);
+                
+                     }
+        }
+    }
+
+    /**
+     * Adds a new customer account with personal and bank details to the array of Customer
+     */
+    static void addCustomer() {
+        Customer c = new Customer(count);
+        c.createCustomerAcc();
+        count++;
+        customerArray.add(c);
+    }
+ /**
+*Deposites money to the specified account stored in the Array
+*/ 
+static void dep() {
+        System.out.print("Enter your Account number : ");
+        int input = s.nextInt();
+         try {
+             customerArray.get(input).deposite();
+        }
+        catch(ArrayIndexOutOfBoundsException b) {
+            System.out.println("Invalid Account number.");
+        }
+    }
+
+    /**
+*Withdraws money from the specified account stored in the Array
+*/   
+static void with() {
+        System.out.print("Please enter your Account number : ");
+        int input = s.nextInt();
+        try {
+             customerArray.get(input).withdraw();
+        }
+        catch(ArrayIndexOutOfBoundsException b) {
+            System.out.println("Invalid Account number.");
+        }
+    }
+
+    /**
+*Transfers money from a specified Account to a Beneficiery's account both stored in the array amd performs required operations on that bank balance details
+*/   
+
+static void transfer() {
+        System.out.print("Enter your account number : ");
+        int inAccountNo = s.nextInt();
+        System.out.print("Enter the Account number to which u ant to transfer Money : ");
+        int outAccountNo=s.nextInt();
+        try {
+            double CurrentBal = customerArray.get(inAccountNo).bal;
+            double benificiaryBalance = customerArray.get(outAccountNo).bal;
+            System.out.print("Please enter the amount to transfer : ");
+            double transferAmt = s.nextDouble();
+            if((CurrentBal-transferAmt)>= 500) {
+                customerArray.get(inAccountNo).bal -= transferAmt;
+                customerArray.get(outAccountNo).bal += transferAmt;
+                System.out.println("The Money was Transferred Successfully");
+            }
+            else {
+                System.out.println("Insufficent balance. Transfer Failed");
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException b){
+            System.out.println("Invaild Account Numbers!!");
+        }
+    }
+
+    /**
+*Calculates and displays the total bank balance
+*/
+
+static void totalBankBalance() {
+        double totBankBal=0;
+        for(int j=0;j<count;j++)
+        {
+            totBankBal += customerArray.get(j).bal;
+        }
+        
+            System.out.println("The Total Money in bank is : "+totBankBal);
+    }
+
+    /**
+*finds the richest person in the bank with the maximum balance
+*/
+
+static void richestPerson() {
+        try {
+                int richestPersonAccNo=0;
+                double max = customerArray.get(0).bal;
+                for(int j=1;j<count;j++) {
+                    double balance = customerArray.get(j).bal;
+                    if(max<balance){
+                        max = balance;
+                        richestPersonAccNo = j;
+                    }
+                }
+                
+                System.out.println("The person with highest bank balance is : "+customerArray.get(richestPersonAccNo).custName+" With balance : "+max);
+        }
+        catch (ArrayIndexOutOfBoundsException b) {
+            System.out.println("The Bank has No Accounts.");
+        }
+    }
+}
